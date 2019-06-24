ec6edaab6d60ccf204efa3d73085ec26ff05d20d
aman <rahmat.adi.putra.21@gmail.com>
2019-06-24 20:09:45 +0700

    library falcon

--- src/Providers/FalconServiceProvider.php
+++ src/Providers/FalconServiceProvider.php
@@ -34,13 +34,19 @@ class FalconServiceProvider extends ServiceProvider
         // overwrite database default in file /config/database.php
         // Config::set('database.default', env('DB_CONNECTION', 'falcon'));
 
-        $this->publishes([
-            __DIR__.'/../../config/database.php' => base_path('config/database.php'),
-        ], 'config');
+        // $this->publishes([
+        //     __DIR__.'/../../config/database.php' => base_path('config/database.php'),
+        // ], 'config');
 
         // $this->publishes([
         //     __DIR__.'/../../database/migrations/create_newname_table.php.stub' => database_path('migrations/'.date('Y_m_d_His', time()).'_create_nename_table.php'),
         // ], 'migrations');
+
+        $this->mergeConfigFrom(__DIR__ . '/../../config/database.php', 'database');
+        
+        $this->publishes([
+            __DIR__ . '/../../config' => base_path('config')
+        ], 'config');
 
         $this->loadRoutesFrom(__DIR__.'/../../routes/api.php');
     }
