b67e6a6e030181ba83ba647943bc2a17cdd63603
aman <rahmat.adi.putra.21@gmail.com>
2019-06-25 12:50:58 +0700

    library falcon

--- src/Providers/FalconServiceProvider.php
+++ src/Providers/FalconServiceProvider.php
@@ -40,15 +40,15 @@ class FalconServiceProvider extends ServiceProvider
 
         $this->publishes([
             __DIR__.'/../../database/migrations/create_users_table.php' => database_path('migrations/2014_10_12_000000_create_users_table.php'),
-            __DIR__.'/../../database/migrations/create_logs_table.php' => database_path('migrations/'.date('Y_m_d').'_000002_create_logs_table.php'),
-            __DIR__.'/../../database/migrations/create_settings_table.php' => database_path('migrations/'.date('Y_m_d').'_000003_create_settings_table.php'),
-            __DIR__.'/../../database/migrations/create_applications_table.php' => database_path('migrations/'.date('Y_m_d').'_000004_create_applications_table.php'),
-            __DIR__.'/../../database/migrations/create_companies_table.php' => database_path('migrations/'.date('Y_m_d').'_000005_create_companies_table.php'),
-            __DIR__.'/../../database/migrations/create_departments_table.php' => database_path('migrations/'.date('Y_m_d').'_000006_create_departments_table.php'),
-            __DIR__.'/../../database/migrations/create_roles_table.php' => database_path('migrations/'.date('Y_m_d').'_000007_create_roles_table.php'),
-            __DIR__.'/../../database/migrations/create_user_has_roles_table.php' => database_path('migrations/'.date('Y_m_d').'_000008_create_user_has_roles_table.php'),
-            __DIR__.'/../../database/migrations/create_permissions_table.php' => database_path('migrations/'.date('Y_m_d').'_000009_create_permissions_table.php'),
-            __DIR__.'/../../database/migrations/create_role_has_permissions_table.php' => database_path('migrations/'.date('Y_m_d').'_000010_create_role_has_permissions_table.php'),
+            __DIR__.'/../../database/migrations/create_logs_table.php' => database_path('migrations/'.date('Y_m_d').'_000001_create_logs_table.php'),
+            __DIR__.'/../../database/migrations/create_settings_table.php' => database_path('migrations/'.date('Y_m_d').'_000002_create_settings_table.php'),
+            __DIR__.'/../../database/migrations/create_applications_table.php' => database_path('migrations/'.date('Y_m_d').'_000003_create_applications_table.php'),
+            __DIR__.'/../../database/migrations/create_companies_table.php' => database_path('migrations/'.date('Y_m_d').'_000004_create_companies_table.php'),
+            __DIR__.'/../../database/migrations/create_departments_table.php' => database_path('migrations/'.date('Y_m_d').'_000005_create_departments_table.php'),
+            __DIR__.'/../../database/migrations/create_roles_table.php' => database_path('migrations/'.date('Y_m_d').'_000006_create_roles_table.php'),
+            __DIR__.'/../../database/migrations/create_user_has_roles_table.php' => database_path('migrations/'.date('Y_m_d').'_000007_create_user_has_roles_table.php'),
+            __DIR__.'/../../database/migrations/create_permissions_table.php' => database_path('migrations/'.date('Y_m_d').'_000008_create_permissions_table.php'),
+            __DIR__.'/../../database/migrations/create_role_has_permissions_table.php' => database_path('migrations/'.date('Y_m_d').'_000009_create_role_has_permissions_table.php'),
         ], 'falcon-migrations');
 
         $this->loadRoutesFrom(__DIR__.'/../../routes/api.php');
