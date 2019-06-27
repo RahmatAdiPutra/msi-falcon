e59c397d0ce5622bba03d8620b9682bb2c8affa9
aman <rahmat.adi.putra.21@gmail.com>
2019-06-27 19:02:36 +0700

    library falcon

--- src/Console/Commands/FalconCommand.php
+++ src/Console/Commands/FalconCommand.php
@@ -14,7 +14,7 @@ class FalconCommand extends Command
      *
      * @var string
      */
-    protected $signature = 'falcon:start {connection : Choose connection for migrate}';
+    protected $signature = 'falcon:start {connection? : Choose connection for migrate}';
 
     /**
      * The console command description.
