d343a59be6f7d8129f9718781d2689c7623bc1b7
aman <rahmat.adi.putra.21@gmail.com>
2019-06-27 13:37:14 +0700

    library falcon

--- src/Providers/FalconServiceProvider.php
+++ src/Providers/FalconServiceProvider.php
@@ -61,6 +61,8 @@ class FalconServiceProvider extends ServiceProvider
         ], 'falcon-migrations');
 
         Artisan::call('vendor:publish', ['--tag' => ['falcon-config', 'falcon-migrations']]);
+
+        $this->loadMigrationsFrom(__DIR__.'/../../../../../database/migrations/');
     }
 
     public function setupMigrations()
